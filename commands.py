60f355000ee9579d7d4bda128d0378a2e7890b79
goku <wkurts247@gmail.com>
2023-09-07 11:58:46 +0300

    added all clis

--- commands.py
+++ commands.py
@@ -19,6 +19,45 @@ def add_venue(name, capacity):
     session.close()
     click.echo(f"Added venue: {name}")
 
+@cli.command()
+@click.option("--name", prompt="Event name", help="Name of the event")
+@click.option("--venue", prompt="Venue name", help="Name of the venue")
+@click.option("--event-date", prompt="Event date (YYYY-MM-DD)", help="Date of the event in format YYYY-MM-DD")
+def add_event(name, venue, event_date):
+    session = SessionLocal()
+    venue_obj = session.query(Venue).filter_by(name=venue).first()
+    if venue_obj:
+        try:
+            event_date = datetime.strptime(event_date, "%Y-%m-%d")
+        except ValueError:
+            click.echo("Invalid date format. Please use YYYY-MM-DD.")
+            session.close()
+            return
+        
+        event = Event(name=name, venue=venue_obj, date_time=event_date)
+        session.add(event)
+        session.commit()
+        session.close()
+        click.echo(f"Created event: {name} at {venue} on {event_date.strftime('%Y-%m-%d')}")
+    else:
+        click.echo(f"Venue {venue} not found.")
+
+@cli.command()
+@click.option("--user", prompt="Your name", help="Your name for the booking")
+@click.option("--event", prompt="Event name", help="Name of the event")
+def book_event(user, event):
+    session = SessionLocal()
+    event_obj = session.query(Event).filter_by(name=event).first()
+    if event_obj:
+        event_date = event_obj.date_time.strftime("%Y-%m-%d")
+        booking = Booking(user_name=user, event=event_obj)
+        session.add(booking)
+        session.commit()
+        session.close()
+        click.echo(f"Booking made for {event} by {user} on {event_date}")
+    else:
+        click.echo(f"Event {event} not found.")
+
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     cli()
